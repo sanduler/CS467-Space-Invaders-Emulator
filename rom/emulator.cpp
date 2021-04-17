4b1e375cbef637ae0c0e6299378fe9e91b34f4ec
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-04-17 16:42:51 -0700

    implemented the setup for the emulator and the start of the emulator

--- rom/emulator.cpp
+++ rom/emulator.cpp
@@ -5,6 +5,13 @@
 #include "CPU.h"
 #include "emulator.h"
 
+/****************************************
+ * Functions bellow deal with the interrupt
+ * 
+ * 
+ * 
+ * **************************************/
+
 void createInterrupt(current_8080 *state, int flag_interrupt)
 {
     //perform PUSH PC
@@ -18,7 +25,23 @@ void createInterrupt(current_8080 *state, int flag_interrupt)
     //enable the state (0 ->1)  
 }
 
-int GetFileSize(char*file) 
+int64_t getTime()
+{
+	//struct to get the time
+    struct timespec tms;
+
+    //returns the time of the clock cycle
+}
+
+
+/****************************************
+ * Functions bellow deal with the rom file
+ * 
+ * 
+ * 
+ * **************************************/
+
+int fileSize(char*file) 
 {
 
 	//open rom file
@@ -36,7 +59,9 @@ int GetFileSize(char*file)
 
 current_8080* LoadROM(current_8080 *state, char*file, uint16_t location) 
 {
+
     //if file is not found then seek the file and print the error message
+
 
     //get the size from getSize and compare
 
@@ -52,4 +77,62 @@ current_8080* LoadROM(current_8080 *state, char*file, uint16_t location)
 
     //return the state of the rom when its loaded
    
+}
+
+/****************************************
+ * Functions bellow deal with the starting
+ * running and functioning of the emulator
+ * 
+ * 
+ * **************************************/
+
+//sets up the emulator initileses all the states and codes to false
+current_8080 setEmulator()
+{
+	ConditionCodes cc;
+
+	//sets the condition codes to 0 --> false
+	c_Codes.z = 0;
+    c_Codes.s = 0;
+    c_Codes.p = 0;
+    c_Codes.cy = 0;
+    c_Codes.ac = 0;
+
+
+    //sets the current state of the emulator to 0 --> false
+    current_8080 state;
+    state.a = 0;
+    state.b = 0;
+    state.c = 0;
+    state.d = 0;
+    state.e = 0;
+    state.h = 0;
+    state.l = 0;
+    state.sp = 0;
+    state.pc = 0;
+    state.int_enable = 0;
+    size_t max_size = 1 << 15;
+
+
+    //returns the state
+    return state;
+}
+
+//starts the emulator
+void startEmulator(char*file)
+{
+	current_8080 state = startEmulator();
+	LoadROM(&state, file, 0x0);
+
+	int cyclesCPU = 0;
+    int speedClock = 2e6 / 120;
+    int fsize = fileSize(file);
+
+	uint64_t cur_time = getTime();
+    if (!state.int_enable) 
+    {
+        startTime = cur_time;
+    }
+
+cyclesCPU += NextInstruction(&state, &inp);
 }
--- rom/emulator.h
+++ rom/emulator.h
@@ -12,14 +12,20 @@
 void startEmulator(char*file);
 
 //check the file size and if the file is found
-int fileSize(char*file) 
+int fileSize(char*file); 
 
-current_8080 startEmulator();
+current_8080 setEmulator();
 
 //Load the rom into the current state and location
 //use pointer to find the address location of the state and pointer to find the file in the
 //memory
 current_8080* LoadROM(current_8080 *state, char*file, uint16_t location);
+
+//gets the time of the clock cycle
+int64_t getTime();
+
+//start the emulator function
+void startEmulator(char*file);
 
 
 #endif
