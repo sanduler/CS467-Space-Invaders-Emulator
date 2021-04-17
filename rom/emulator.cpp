28e18dcf481c9c8d5523176010fa3f7a14194e5b
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-04-17 16:28:25 -0700

    file size and loading the rom

--- rom/emulator.cpp
+++ rom/emulator.cpp
@@ -17,3 +17,39 @@ void createInterrupt(current_8080 *state, int flag_interrupt)
 
     //enable the state (0 ->1)  
 }
+
+int GetFileSize(char*file) 
+{
+
+	//open rom file
+
+    
+    //if the file size does not match
+
+
+    //close the file
+
+
+    //return file size
+
+}
+
+current_8080* LoadROM(current_8080 *state, char*file, uint16_t location) 
+{
+    //if file is not found then seek the file and print the error message
+
+    //get the size from getSize and compare
+
+
+    // read in file
+
+
+    //write in memort the location and the state.
+
+
+    //print message that the rom is loaded
+
+
+    //return the state of the rom when its loaded
+   
+}
--- rom/emulator.h
+++ rom/emulator.h
@@ -11,10 +11,14 @@
 //Function used to start the emulator. import a file 
 void startEmulator(char*file);
 
+//check the file size and if the file is found
+int fileSize(char*file) 
 
 current_8080 startEmulator();
 
 //Load the rom into the current state and location
+//use pointer to find the address location of the state and pointer to find the file in the
+//memory
 current_8080* LoadROM(current_8080 *state, char*file, uint16_t location);
 
 
