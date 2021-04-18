d938dbc89afd84a0bd7faa4e0ea899f1b91ca1a1
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-04-18 00:01:01 -0700

    created the CPU class for the emulator

---
+++ CPU.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Ruben Sanduleac on 4/17/21.
+//
+
+#include "CPU.h"
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
---
+++ CPU.h
@@ -0,0 +1,15 @@
+//
+// Created by Ruben Sanduleac on 4/17/21.
+//
+
+#ifndef PROJECT_CPU_H
+#define PROJECT_CPU_H
+
+
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+
+#endif
