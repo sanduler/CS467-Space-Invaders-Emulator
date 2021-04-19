6e52e350abc01bd35a411085b2e6ac32aa401890
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-04-19 13:36:34 -0700

    Update i8080.cpp

--- i8080.cpp
+++ i8080.cpp
@@ -2,6 +2,7 @@
 // Created by Ruben Sanduleac on 4/18/21.
 //
 
+#include <iostream>
 #include "i8080.h"
 #include <cstdint>
 #include <cstdio>
@@ -56,7 +57,7 @@ uint8_t errorParity(uint16_t value)
 
 /*****************************
  * Function Type: bool, checks if true
- * This function takes the bits balue to comapre the sizes
+ * Discription: This function takes the bits balue to comapre the sizes
  * parity
  * this is done by checking the lower and upper limits of the bit value and com
  * comapring them using the modulus operator
@@ -77,6 +78,72 @@ bool parity(uint8_t b_bits)
 
     //return the bit value
     return ((lowerbit % 2) == 0);
+}
+
+/*****************************
+ * Function Type: constructor: i8080::i8080(size_t memorySize, uint16_t beginPoint)
+ * Discription: This function allocates memory and sets the size
+ * depending on the rom file then sets the starting point in 
+ * memory
+ * **************/
+i8080::i8080(size_t memorySize, uint16_t beginPoint)
+{
+    //Find in memory
+    this->memsize = memorySize;
+
+    //allocate the memory set in specific point in memomory in an array 
+    
+    memory = new uint8_t[memorySize];
+    
+    PC = beginPoint;
+
+}
+
+/*****************************
+ * Function Type: destructor, ~i8080();
+ * Discription: clears the allocated memory 
+ * **************/
+i8080::~i8080()
+{
+    delete[] memory;
+}
+
+/*****************************
+ * Function Type: int i8080::loadRom(const char * nameOfFile, size_t offset)
+ * Discription: gets the name of the rom, loads the rom into the meormy and then
+ *              checks if its loaded iof not breaks the loop and returns false 
+ * **************/
+
+int i8080::loadRom(const char * nameOfFile, size_t offset)
+{
+
+    //sets the size in memory to 0 = false
+    size_t s = 0;
+
+    //load the rom in usign ifstream 
+    ifstream rom(nameOfFile, ios::binary);
+    
+    //use if statement to check if rom is loaded or rom exists
+    if (!rom)
+    {
+        cout << "No rom loaded, please try again" << endl;
+        return s;
+    }
+
+    //While loop, used when the rom does exists loads the rom into the memor
+    while (true)
+    {
+        if ((offset + s) >= memsize)
+            break;
+
+        //get the rom
+        auto i = rom.get();
+        if (i == EOF)
+            break;
+        memory[offset + s] = i;
+        ++s;
+    }
+    return s;
 }
 
 
