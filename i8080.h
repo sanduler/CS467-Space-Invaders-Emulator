d45a946da467e5d6ee333387d56b17996c9eacc6
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-04-23 11:23:32 -0700

    updated to match class sstructure

--- i8080.h
+++ i8080.h
@@ -1,72 +1,111 @@
-//
-// Created by Ruben Sanduleac on 4/20/21.
-//
+/*********************************
+ * i8080 Emulator
+ * - Flags Class
+ * - Register Class
+ * - State Class
+ * - i8080 Class
+ ********************************/
 
-#ifndef I8080_H
-#define I8080_H
+#ifndef PROJECT101_I8080_H
+#define PROJECT101_I8080_H
+#include <cstdint>
 #include <stdint.h>
-#include "memory.h"
-struct i8080
-{
-    i8080(memory* mem, uint8_t _ConsoleMode);
+#include <opencl-c-base.h>
+
+
+/*********************************
+ * Flags class
+ * Sets the flags for the opcodes
+ ********************************/
+class i8080_Flags{
+private:
+public:
+    i8080_Flags();
+    ~i8080_Flags();
+    bool S;
+    bool Z;
+    bool ZeroValue = 0;
+    bool OneValue = 1;
+    bool AC();
+    double get_AC();
+    double set_AC();
+    bool unset_AC();
+    // Flags
+    uint8_t FlagZSP[0x100]; // Precalculated ZSP
+    uint8_t flag_Z; // Zero
+    uint8_t flag_S; // Sign
+    uint8_t flag_P; // Parity
+    uint8_t flag_C; // Carry
+    uint8_t flag_AC; // Auxiliar Carry
+};
+
+/*********************************
+ * Registers class
+ * Intializes the Registers
+ ********************************/
+class i8080_Registers {
+private:
+public:
+    uint16 A;
+    uint8 B;
+    uint8 C;
+    uint16 PC;
+    uint16 SP;
+
+    double unint8_A;
+    double unint_get_A();
+    void set_A(int A);
+    int unint_PC();
+    int get_PC();
+    void set_PC(int PC);
+
+    uint8_t* reg_M;
+    uint8_t true_reg_A;
+    uint8_t* reg_A = &true_reg_A; // Pointer uniformity
+    uint16_t reg_BC;
+    uint8_t* reg_B = ((uint8_t*) &reg_BC) + 1;
+    uint8_t* reg_C = ((uint8_t*) &reg_BC);
+    uint16_t reg_DE;
+    uint8_t* reg_D = ((uint8_t*) &reg_DE) + 1;
+    uint8_t* reg_E = ((uint8_t*) &reg_DE);
+    uint16_t reg_HL;
+    uint8_t* reg_H = ((uint8_t*) &reg_HL) + 1;
+    uint8_t* reg_L = ((uint8_t*) &reg_HL);
+};
+
+/*********************************
+ * State class
+ * Gets the state of the emulator
+ * uses the flags and registers objects
+ * as members
+ ********************************/
+class i8080_State {
+private:
+public:
+    i8080_State();
+    ~i8080_State();
+    i8080_Flags flags;
+    i8080_Registers registers;
+    int unint_SP();
+    double get_SP();
+    void set_SP(int SP);
+    int unset_AC();
+};
+
+/*********************************
+ * i8080 class
+ * recieves the state class as a member
+ * loads the rom and intilizes the emulator
+ ********************************/
+class i8080 {
+private:
+    i8080_State state;
+public:
+    i8080();
+    ~i8080();
     unsigned char ROM;
     double memory;
-    i8080_state state;
-};
-struct i8080_state
-{
-        int unint_SP();
-        double get_SP();
-        void set_SP(int SP);
-        int unset_AC();
-        state_Flags flags;
-};
-struct state_Flags
-{
-             bool AC();
-            double get_AC();
-            double set_AC();
-            bool unset_AC();
-            // Flags
-            uint8_t FlagZSP[0x100]; // Precalculated ZSP
-            uint8_t flag_Z; // Zero
-            uint8_t flag_S; // Sign
-            uint8_t flag_P; // Parity
-            uint8_t flag_C; // Carry
-            uint8_t flag_AC; // Auxiliar Carry
 };
 
-        struct Inputs
-        {
-            int bit[256];
-            int get_Inputs();
-        };
 
-        struct Registers
-        {
-            double unint8_A;
-            double unint_get_A();
-            void set_A(int A);
-            int unint_PC();
-            int get_PC();
-            void set_PC(int PC);
-
-            uint8_t* reg_M;
-            uint8_t true_reg_A;
-            uint8_t* reg_A = &true_reg_A; // Pointer uniformity
-            uint16_t reg_BC;
-            uint8_t* reg_B = ((uint8_t*) &reg_BC) + 1;
-            uint8_t* reg_C = ((uint8_t*) &reg_BC);
-            uint16_t reg_DE;
-            uint8_t* reg_D = ((uint8_t*) &reg_DE) + 1;
-            uint8_t* reg_E = ((uint8_t*) &reg_DE);
-            uint16_t reg_HL;
-            uint8_t* reg_H = ((uint8_t*) &reg_HL) + 1;
-            uint8_t* reg_L = ((uint8_t*) &reg_HL);
-            uint16_t PC, SP;
-        };
-    };
-
-};
-
-#endif //I8080_H
+#endif //PROJECT101_I8080_H
