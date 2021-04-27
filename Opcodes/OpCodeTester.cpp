90d7ce04604d3b6672eef2af41ee3cd73ee1f66e
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-04-27 13:21:55 -0700

    first tests for loading rom into loadRom function

--- Opcodes/OpCodeTester.cpp
+++ Opcodes/OpCodeTester.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include "i8080_struct.h"
+using namespace std;
 
 i8080_CPU i8080_control;
 //i8080_CPU i8080_variable;
@@ -20,5 +21,14 @@ int main(int argc, char* argv[])
     printf("Hello World\n");
     printf("flag_Z:");
     printf("%d\n", i8080_control.state.flags.flag_Z);
+
+    //loads the room into the loadRom function
+    int bl;
+    
+    bl = i8080_control.loadRom("rom/invaders.h", 0);
+    bl += i8080_control.loadRom("rom/invaders.g", 0x0800);
+    bl += i8080_control.loadRom("rom/invaders.f", 0x1000);
+    bl += i8080_control.loadRom("rom/invaders.e", 0x1800);
+
     return 0;
 }
