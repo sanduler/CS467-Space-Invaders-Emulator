c87cc9d55d8e3320ea265d99608800af66e6448a
mkmatt714 <matthewm@oregonstate.edu>
2021-05-01 16:21:35 -0700

    Getting in sync

--- Opcodes/OpCodeFunctions_general.cpp
+++ Opcodes/OpCodeFunctions_general.cpp
@@ -105,7 +105,7 @@ void func_DCR_Registers(i8080_Register &reg_Source)
 	i8080.state.flag_S.set(func_Check_Sign(uint8_ResultTemp));
 	i8080.state.flag_Z.set(func_Check_Zero(uint8_ResultTemp));
 	// When checking the Auxiliary Carry Bit Source2 needs to be a 2's compliment
-	i8080.state.flag_AC.set(func_Check_AuxCarry(uint8_RegisterTemp, ~(0x01) + 0x01));
+	i8080.state.flag_AC.set(func_Check_AuxCarry(uint8_RegisterTemp, 0xFF));
 	
 	i8080.state.flag_P.set(func_Check_Parity(uint8_ResultTemp));
 	
@@ -358,7 +358,7 @@ bool func_Check_Zero(uint8_t uint8_Register)
 // Generic Check Auxiliary Carry Function to return if there was a carry from the bit position 3 addition/subtraction/etc.
 bool func_Check_AuxCarry(uint8_t uint8_Source1, uint8_t uint8_Source2)
 {
-	bool boolResult = 0;
+	bool boolResult = false;
 	
 	uint8_t uint8_Carry = 0x00;
 	
@@ -380,7 +380,7 @@ bool func_Check_AuxCarry(uint8_t uint8_Source1, uint8_t uint8_Source2)
 		uint8_ResultTemp = uint8_Source1Temp + uint8_Source2Temp + uint8_Carry;
 		
 		if ((uint8_ResultTemp & 0x02) != 0x00){
-			boolResult = 1;
+			boolResult = true;
 		}
 		else {
 			boolResult = 0;
