65d39b7396aa69d1d617f00109c3ce720098dca2
woym97 <woym@oregonstate.edu>
2021-05-03 19:57:31 -0400

    Fixed capitalization

--- Opcodes/Run_OpCode_Test.sh
+++ Opcodes/Run_OpCode_Test.sh
@@ -2,13 +2,13 @@
 
 if [ $1 == "p" ]
 then
-    g++ -DPRINT_OPT=2 OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpcodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
+    g++ -DPRINT_OPT=2 OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpCodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
 elif [ $1 == "f" ]
 then
-    g++ -DPRINT_OPT=3 OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpcodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
+    g++ -DPRINT_OPT=3 OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpCodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
 elif [ $1 == "u" ]
 then
-    g++ -DPRINT_OPT=4 OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpcodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
+    g++ -DPRINT_OPT=4 OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpCodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
 else
     g++ OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpcodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
 fi
