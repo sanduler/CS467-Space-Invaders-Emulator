5530ffb29e0998596b5da7fb901011d3666a2a7f
woym97 <woym@oregonstate.edu>
2021-05-03 19:59:15 -0400

    Fixed filename in script

--- Opcodes/Run_OpCode_Test.sh
+++ Opcodes/Run_OpCode_Test.sh
@@ -10,6 +10,6 @@ elif [ $1 == "u" ]
 then
     g++ -DPRINT_OPT=4 OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpCodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
 else
-    g++ OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpcodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
+    g++ OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpCodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
 fi
 ./proj 
