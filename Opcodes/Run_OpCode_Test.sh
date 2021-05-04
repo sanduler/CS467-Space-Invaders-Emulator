2fe266ce3e1d6b4c4ee8e3b129aa6c860f89d3c5
woym97 <woym@oregonstate.edu>
2021-05-03 20:04:29 -0400

    added -std=c++11 flag to compiler

--- Opcodes/Run_OpCode_Test.sh
+++ Opcodes/Run_OpCode_Test.sh
@@ -2,14 +2,14 @@
 
 if [ $1 == "p" ]
 then
-    g++ -DPRINT_OPT=2 OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpCodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
+    g++ -DPRINT_OPT=2 OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpCodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm -std=c++11
 elif [ $1 == "f" ]
 then
-    g++ -DPRINT_OPT=3 OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpCodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
+    g++ -DPRINT_OPT=3 OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpCodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm -std=c++11
 elif [ $1 == "u" ]
 then
-    g++ -DPRINT_OPT=4 OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpCodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
+    g++ -DPRINT_OPT=4 OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpCodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm -std=c++11
 else
-    g++ OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpCodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm 
+    g++ OpCodeTester.cpp ../i8080_struct.cpp OpCodeFunctions.cpp OpCodeFunctions_general.cpp OpCodeCaller.cpp -o proj -lm -std=c++11
 fi
 ./proj 
